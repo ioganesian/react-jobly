277c399ebef7bb02a304b56647bd44efd4296171
Igor Oganesian <igor.oganesian@gmail.com>
2023-11-07 16:29:17 -0500

    application state

--- src/JobCard.js
+++ src/JobCard.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./JobCard.css";
+import userContext from "./userContext";
 
 /** Shows limited information about a job
  *
  * Props: {id, title, company, salary, equity }
  *
- * State: None
+ * State: applied (user has applied to job)
  *
  * JobCardList --> JobCard
  */
 
 function JobCard({ id, title, company, salary, equity }) {
+
+  const { hasAppliedToJob, applyToJob } = useContext(userContext);
+  const [applied, setApplied] = useState();
+
+  useEffect(
+    function updateAppliedStatus() {
+      setApplied(hasAppliedToJob(id));
+    },
+    [id, hasAppliedToJob]
+  );
+
+  /** Apply for a job */
+  async function handleApply(evt) {
+    if (hasAppliedToJob(id)) return;
+    applyToJob(id);
+    setApplied(true);
+  }
+
   return (
     <div className="JobCard">
       <h6 className="JobCard-title">{title}</h6>
       <p>{company}</p>
       <p>Salary: {salary}</p>
       <p>Equity: {equity}</p>
+      <button
+          className="btn btn-danger fw-bold text-uppercase float-end"
+          onClick={handleApply}
+          disabled={applied}
+        >
+          {applied ? "Applied" : "Apply"}
+        </button>
     </div>
   );
 }
